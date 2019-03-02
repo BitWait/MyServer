bcef5efd2058694ecec11081a0c6118f641ee5ba
BitWait <imlvyong@163.com>
2019-03-02 13:37:43 +0800

    push the VS project

---
+++ server/server/base/CMakeFiles/timestamp_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/timestamp_test.dir/timestamp.cpp.o"
+  "CMakeFiles/timestamp_test.dir/Timestamp_test.cpp.o"
+  "timestamp_test.pdb"
+  "timestamp_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/timestamp_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
