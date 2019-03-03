3e2a2ff77389ddfedff08de044578c4072748ba4
BitWait <imlvyong@163.com>
2019-03-03 12:21:30 +0800

    alter LogStream

--- base/LogStream.h
+++ base/LogStream.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <assert.h>
 #include <string.h>
+#include <string>
 
 namespace detail
 {
@@ -24,6 +26,14 @@ namespace detail
 			cookie_ = cookie; 
 		}
 
+		const char * data(){ return data_; }
+		int length()const{ return static_cast<int>(cur_ - data_); }
+		char * current(){ return cur_; }
+		void add(size_t len){ cur_ += len; }
+
+		void reset(){ cur_ = data_; }
+		void bzero(){ ::bzero(data_, sizeof data_); }
+
 		void append(const char * buf, size_t len)
 		{
 			if (avail() > static_cast<int>(len))
@@ -33,7 +43,9 @@ namespace detail
 			}
 		}
 
+		const char * debugString();
 
+		std::string asString()const{ return std::string(data_, length()); }
 		// write to data_ directly
 		int avail()const
 		{
@@ -66,8 +78,83 @@ public:
 		return *this;
 	}
 
+
+	self& operator<<(short);
+	self& operator<<(unsigned short);
+	self& operator<<(int);
+	self& operator<<(unsigned int);
+	self& operator<<(long);
+	self& operator<<(unsigned long);
+	self& operator<<(long long);
+	self& operator<<(unsigned long long);
+
+
+	self& operator<<(const void*);
+	self& operator<<(char v)
+	{
+		buffer_.append(&v, 1);
+		return *this;
+	}
+
+	self& operator<<(float v)
+	{
+		*this << static_cast<double>(v);
+		return *this;
+	}
+	self& operator<<(double);
+
+	self& operator<<(const char* str)
+	{
+		if (str)
+		{
+			buffer_.append(str, strlen(str));
+		}
+		else
+		{
+			buffer_.append("(null)", 6);
+		}
+		return *this;
+	}
+
+	self& operator<<(const unsigned char* str)
+	{
+		return operator<<(reinterpret_cast<const char*>(str));
+	}
+
+	self& operator<<(const std::string& v)
+	{
+		buffer_.append(v.c_str(), v.size());
+		return *this;
+	}
+
+	void append(const char *data, int length){ buffer_.append(data, length); }
+	const Buffer& buffer()const{ return buffer_; }
+	void resetBuffer(){ buffer_.reset(); }
 private:
+	void staticCheck();
+	template<typename T>
+	void formatInteger(T);
 	Buffer buffer_;
 	static const int kMaxNumericSize = 32;
 };
+
+class Fmt
+{
+public:
+	template<typename T>
+	Fmt(const char* fmt, T val);
+
+	const char * data()const{ return buf_; }
+	int length()const{ return length_; }
+private:
+	char buf_[32];
+	int length_;
+};
+
+
+inline LogStream& operator<<(LogStream& s, const Fmt& fmt)
+{
+	s.append(fmt.data(), fmt.length());
+	return s;
+}
 
--- base/LogStream_test.cpp
+++ base/LogStream_test.cpp
@@ -1,9 +1,45 @@
 #include "LogStream.h"
-
+#include <string>
+#include <iostream>
 using namespace detail;
 int main()
 {
 	LogStream os;
-	//os << 1;
-	
+	const LogStream::Buffer &buf = os.buffer();
+	std::cout << "------------------begin test bool----------------------" << std::endl;
+	os << true;
+	std::cout << buf.asString().c_str() << std::endl;
+
+	os << false;
+	std::cout << buf.asString().c_str() << std::endl;
+
+	os.resetBuffer();
+	//std::cout << buf.asString().c_str() << std::endl;
+
+	std::cout << "------------------end test bool----------------------" << std::endl;
+
+	std::cout << "------------------begin test integer----------------------" << std::endl;
+	os<< 1;
+	std::cout << buf.asString().c_str() << std::endl;
+	os << 0;
+	std::cout << buf.asString().c_str() << std::endl;
+	os << -1;
+	std::cout << buf.asString().c_str() << std::endl;
+	os.resetBuffer();
+	//std::cout << buf.asString().c_str() << std::endl;
+	std::cout << "------------------end test integer----------------------" << std::endl;
+
+	std::cout << "------------------begin test fmt----------------------" << std::endl;
+	os << Fmt("%4d", 1);
+	std::cout << buf.asString().c_str() << std::endl;
+	os.resetBuffer();
+
+	os << Fmt("%4.2f", 1.2);
+	std::cout << buf.asString().c_str() << std::endl;
+	os.resetBuffer();
+
+	os << Fmt("%4.2f", 1.2) << Fmt("%4d", 43);
+	std::cout << buf.asString().c_str() << std::endl;
+	os.resetBuffer();
+	std::cout << "------------------end test fmt----------------------" << std::endl;
 }
